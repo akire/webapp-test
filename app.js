e2479e8dffe066b4454381eea0469ed58a5bf6e4
Erika Thorsen <erika@akire.se>
2012-04-24 12:51:41 +0000

    Set versions for both node and npm for Herokus sake

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 "use strict";
 
 var express = require('express');
+var http = require('http');
 
 function startServer() {    
-    var app = express.createServer(app);
+    var app = express();
+    var server = http.createServer(app);
 
+    app.use(express.static(__dirname + '/'));
+    
     app.get('/', function(request, response) {
         response.redirect('/clock.html');
     });
     
-    app.listen(process.env.PORT || 3000);
+    server.listen(process.env.PORT || 3000);
     console.log("Server has started.");
 }
 
